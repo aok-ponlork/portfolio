7e3d3a7aa99cd779f045bf95916d8bf3f7c22a46
Aok ponlork <ponlorkboss@gmail.com>
2025-07-08 16:00:11 +0700

    #update blog component

--- src/app/modules/blog/blog.component.ts
+++ src/app/modules/blog/blog.component.ts
@@ -4,20 +4,39 @@ import { FontAwesomeShareModule } from '../../shared/modules/font-awesome.module
 import { LottieCoreComponent } from '../../shared/components/lottie/lottie.component';
 import { NzToolTipModule } from 'ng-zorro-antd/tooltip';
 import { UserPreferenceService } from '../../core/services/common/user-preference.service';
+import { BlogCardComponent } from './blog-card/blog-card.component';
+import { Article } from '../../core/models/blogs/blog.model';
+import { ARTICLES } from '../../../assets/data/blog.data';
 
 @Component({
   selector: 'app-blog',
   standalone: true,
-  imports: [
-    CommonModule,
-    FontAwesomeShareModule,
-    LottieCoreComponent,
-    NzToolTipModule,
-  ],
+  imports: [CommonModule, FontAwesomeShareModule, NzToolTipModule, LottieCoreComponent, BlogCardComponent],
   templateUrl: './blog.component.html',
   styleUrl: './blog.component.css',
 })
 export class BlogComponent implements OnInit {
+  articles: Article[] = [];
+  filteredArticles: Article[] = [];
+  activeFilter: string = 'All';
   constructor(public userPref: UserPreferenceService) {}
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.articles = ARTICLES;
+    this.filteredArticles = this.articles;
+  }
+  get articleFilters(): string[] {
+    const allTech = this.articles.flatMap((item) => item.type);
+    return ['All', ...new Set(allTech)];
+  }
+  filterArticle(type: string): void {
+    this.activeFilter = type;
+
+    if (type === 'All') {
+      this.filteredArticles = this.articles;
+    } else {
+      this.filteredArticles = this.articles.filter((item) =>
+        item.type.includes(type)
+      );
+    }
+  }
 }
