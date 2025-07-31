98c203ac0d978ae3a680907f43c2b1646d453613
Aok ponlork <ponlorkboss@gmail.com>
2025-07-31 14:29:44 +0700

    Update blog articles data and add new entry

--- src/assets/data/blog.data.ts
+++ src/assets/data/blog.data.ts
@@ -3,19 +3,35 @@ import { Article } from '../../app/core/models/blogs/blog.model';
 export const ARTICLES: Article[] = [
   {
     id: '1',
-    title: 'How I Built My Angular Dashboard',
+    title: 'Redis Command',
     shortDescription:
-      'A deep dive into the Angular dashboard project I created.',
+      'A practical guide to Redis commands for fast data operations.',
     description:
-      'This article explains the architecture, tech stack, and challenges I faced while building an Angular dashboard that integrates with Firebase and .NET backend. It includes code snippets and lessons learned.',
-    type: ['Backend', 'Backend'],
+      'This article covers essential Redis commands, their use cases, and best practices to efficiently manage in-memory data storage for caching, messaging, and real-time applications.',
+    type: ['Database', 'Backend'],
     images: [
       {
-        url: '/assets/images/dummy.jpg',
-        alt: 'Angular dashboard overview',
+        url: '/assets/articles/images/redis.webp',
+        alt: 'Redis',
       },
     ],
     createdAt: '2025-07-08T12:34:56Z',
-    filePath: '/assets/articles/The_difference_between.md',
+    duration: '3 min',
+  },
+  {
+    id: '2',
+    title: 'How to Create Custom Middleware in .NET and Laravel',
+    shortDescription: 'How to Create Custom Middleware in .NET and Laravel',
+    description:
+      'Learn how to build custom middleware in both .NET and Laravel with step-by-step examples. Master request filtering and reusable logic in your backend apps.',
+    type: ['Backend'],
+    images: [
+      {
+        url: '/assets/articles/images/middleware.webp',
+        alt: 'Middleware',
+      },
+    ],
+    createdAt: '2025-07-08T12:34:56Z',
+    duration: '3 min',
   },
 ];
